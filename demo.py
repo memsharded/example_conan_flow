e9199f9de11c3bc134f278c81f7a815816a4a9b3
memsharded <james@conan.io>
2018-03-28 09:58:22 +0200

    added conan-test

--- demo.py
+++ demo.py
@@ -3,12 +3,13 @@ import os
 
 
 def run(cmd):
-    print("%s\n" % cmd)
+    print("\n%s\n" % cmd)
     ret = os.system(cmd)
     if ret != 0:
         raise Exception("Error running: %s" % cmd)
 
 
+# This should work, but would pollute the current directory with temporary files
 # run("conan source .")
 # run("mkdir build")
 # run("cd build && conan install ..")
@@ -23,7 +24,8 @@ run("conan build   . --source-folder=tmp/source --install-folder=tmp/install --b
 run("conan package . --source-folder=tmp/source --install-folder=tmp/install --build-folder=tmp/build --package-folder=tmp/package")
 # NOTE: prevent ERROR: Package already exists
 run("conan export-pkg . user/testing --source-folder=tmp/source --install-folder=tmp/install --build-folder=tmp/build --force")
-
+# You can also test the package that was just exported
+run("conan test test_package Hello/1.1@user/testing")
 
 # Finally, run a full create, does all of the above + test_package
 run('conan create . user/testing')
